b8864304071a3048044e1a987ab07344f6297d30
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-08-02 21:17:59 -0400

    wrote bash script to make virtual environment

---
+++ make_venv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 -m venv .venv
+source .venv/bin/activate 
+pip install --upgrade pip setuptools wheel --no-cache-dir
+pip install numpy==1.16.4 --no-cache-dir
+pip install tensorflow-gpu==1.13.1 --no-cache-dir
+
