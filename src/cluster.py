67f3327d875bd65497ac64b55b3badee0e9e14cb
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-08-04 14:03:54 -0400

    added comments

--- src/cluster.py
+++ src/cluster.py
@@ -7,6 +7,8 @@ from src.utils import tf_calculate_pairwise_distances, numpy_calculate_distance,
 
 
 class Cluster:
+    """ Defines clusters and methods relevant to building a cluster-tree and for searching the tree. """
+
     def __init__(
             self,
             data: np.memmap,
@@ -21,12 +23,26 @@ class Cluster:
             right=None,
             reading: bool = False,
     ):
+        """ Initializes Cluster object.
+
+        :param data: memmap of points to cluster.
+        :param points: list of indexes of points in the cluster.
+        :param distance_function: distance function to use for the cluster.
+        :param name: name of cluster.
+        :param center: index of center of cluster.
+        :param radius: radius of cluster.
+        :param lfd: local fractal dimension of cluster.
+        :param parent_lfd: local fractal dimension of parent cluster.
+        :param left: left child cluster.
+        :param right: right child cluster.
+        :param reading: weather of not a cluster-tree is being read from file.
+        """
         self.name: str = name
         self.depth: int = len(self.name)
 
         self.data: np.memmap = data
         self.points: List[int] = points
-        self.distance_function: str = distance_function
+        self.df: str = distance_function
         self.parent_lfd: float = parent_lfd
         self.left: Cluster = left
         self.right: Cluster = right
@@ -47,7 +63,11 @@ class Cluster:
         else:
             self.update()
 
-    def _get_potential_centers(self):
+    def _get_potential_centers(self) -> List[int]:
+        """ If the cluster contains too many points, subsample square_root as many points as potential centers.
+
+        :return: list of indexes of potential centers.
+        """
         if self.should_subsample:
             sample_size = int(np.sqrt(len(self.points)))
             points = self.points.copy()
@@ -56,46 +76,63 @@ class Cluster:
         else:
             return self.points.copy()
 
-    def _calculate_pairwise_distances(self):
+    def _calculate_pairwise_distances(self) -> np.ndarray:
+        """ Calculate the pairwise distances between potential centers.
+
+        :return: pairwise distance matrix between potential centers.
+        """
         if self.should_subsample:
             points = np.asarray([self.data[p] for p in self._potential_centers])
-            return tf_calculate_pairwise_distances(a=points, distance_function=self.distance_function)
+            return tf_calculate_pairwise_distances(points, self.df)
         else:
             points = np.asarray([self.data[p] for p in self.points])
-            return numpy_calculate_distance(a=points, b=points, distance_function=self.distance_function)
+            return numpy_calculate_distance(points, points, self.df)
 
-    def _calculate_center(self):
-        sum_distances = np.sum(a=self._pairwise_distances, axis=1)
+    def _calculate_center(self) -> int:
+        """ Calculate the geometric median of the potential centers. This is the center of the cluster.
+
+        :return: index of center of cluster.
+        """
+        sum_distances = np.sum(self._pairwise_distances, axis=1)
         return self._potential_centers[int(np.argmin(sum_distances))]
 
     def _get_batch(self, start):
         num_points = min(start + self.batch_size, len(self.points)) - start
         return np.asarray([self.data[p] for p in self.points[start: start + num_points]])
 
-    def _calculate_radius(self):
+    def _calculate_radius(self) -> float:
+        """ Calculate the radius of the cluster.
+
+        :return: cluster radius.
+        """
         if self.should_subsample:
-            return max(max([max(tf_calculate_distance(self.data[self.center], self._get_batch(i), self.distance_function))
+            return max(max([max(tf_calculate_distance(self.data[self.center], self._get_batch(i), self.df))
                             for i in range(0, len(self.points), self.batch_size)]), 0.0)
         else:
             points = np.asarray([self.data[p] for p in self.points])
-            distances = numpy_calculate_distance(a=self.data[self.center], b=points, distance_function=self.distance_function)
+            distances = numpy_calculate_distance(self.data[self.center], points, self.df)
             return max(max(distances), 0.0)
 
-    def _calculate_lfd(self):
+    def _calculate_lfd(self) -> float:
+        """ Calculate the local fractal dimension of the cluster.
+
+        :return: local fractal dimension of cluster.
+        """
         if self.should_subsample:
             count = sum([sum([1 if d < self.radius / 2 else 0
                               for d in tf_calculate_distance(self.data[self.center],
                                                              self._get_batch(i),
-                                                             self.distance_function)])
+                                                             self.df)])
                          for i in range(0, len(self.points), self.batch_size)])
         else:
             points = np.asarray([self.data[p] for p in self.points])
-            distances = numpy_calculate_distance(self.data[self.center], points, self.distance_function)
+            distances = numpy_calculate_distance(self.data[self.center], points, self.df)
             count = sum([1 if d < self.radius / 2 else 0 for d in distances])
 
         return 0 if count == 0 else np.log2(len(self.points) / count)
 
-    def update(self):
+    def update(self) -> None:
+        """ Updates the relevant private variables of the cluster. """
         self._potential_centers: List[int] = self._get_potential_centers()
         self._pairwise_distances: np.ndarray = self._calculate_pairwise_distances()
 
@@ -104,11 +141,21 @@ class Cluster:
         self.lfd: float = self._calculate_lfd()
         return
 
-    def can_include(self, query: np.ndarray, radius: float = 0):
-        distance = numpy_calculate_distance(query, self.data[self.center], self.distance_function)
+    def can_include(self, query: np.ndarray, radius: float = 0) -> bool:
+        """ Weather or not the query can be included in the cluster.
+
+        :param query: point to check.
+        :param radius: if the point is a search query, add the radius to cluster_radius.
+        :return:
+        """
+        distance = numpy_calculate_distance(query, self.data[self.center], self.df)
         return distance < (self.radius + radius)
 
-    def can_be_popped(self):
+    def can_be_popped(self) -> bool:
+        """ check if the cluster can be popped.
+
+        :return: Weather or not the cluster can be popped.
+        """
         return all((
             len(self.points) > self.min_points,
             self.radius > self.min_radius,
@@ -116,7 +163,17 @@ class Cluster:
             self.lfd < config.LFD_LIMIT or self.lfd > self.parent_lfd,
         ))
 
-    def pop(self):
+    def pop(self) -> None:
+        """ Pop the cluster into left and right children.
+
+        Steps:
+            * Check if cluster has already been popped or if it can be popped.
+            * Find the two potential centers that are the farthest apart and treat them as the left and right poles.
+            * Partition the points in the cluster by which pole they are closer to.
+            * assign the partitioned lists of points to left or right child cluster appropriately.
+
+        :return:
+        """
         if self.left or self.right or (not self.can_be_popped()):
             return
 
@@ -132,15 +189,15 @@ class Cluster:
         if self.should_subsample:
             for i in range(0, len(self.points), self.batch_size):
                 batch = self._get_batch(i)
-                left_distances = numpy_calculate_distance(self.data[left_pole], batch, self.distance_function)
-                right_distances = numpy_calculate_distance(self.data[right_pole], batch, self.distance_function)
+                left_distances = numpy_calculate_distance(self.data[left_pole], batch, self.df)
+                right_distances = numpy_calculate_distance(self.data[right_pole], batch, self.df)
                 [(left_indexes if l < r else right_indexes).append(self.points[i + j])
                  for j, l, r in zip(range(len(batch)), left_distances, right_distances)]
 
         else:
             points = [self.data[p] for p in self.points]
-            left_distances = numpy_calculate_distance(self.data[left_pole], points, self.distance_function)
-            right_distances = numpy_calculate_distance(self.data[right_pole], points, self.distance_function)
+            left_distances = numpy_calculate_distance(self.data[left_pole], points, self.df)
+            right_distances = numpy_calculate_distance(self.data[right_pole], points, self.df)
 
             [(left_indexes if l < r else right_indexes).append(p)
              for l, r, p in zip(left_distances, right_distances, self.points)]
@@ -148,7 +205,7 @@ class Cluster:
         self.left = Cluster(
             data=self.data,
             points=left_indexes,
-            distance_function=self.distance_function,
+            distance_function=self.df,
             name=f'{self.name}1',
             parent_lfd=self.lfd,
         )
@@ -156,35 +213,44 @@ class Cluster:
         self.right = Cluster(
             data=self.data,
             points=right_indexes,
-            distance_function=self.distance_function,
+            distance_function=self.df,
             name=f'{self.name}2',
             parent_lfd=self.lfd,
         )
 
         return
 
-    def make_tree(self):
+    def make_tree(self) -> None:
+        """ Build cluster tree starting at the current cluster. """
         self.pop()
         if self.left or self.right:
             self.left.make_tree()
             self.right.make_tree()
         return
 
-    def search(self, query: np.ndarray, radius: float, max_depth: int) -> List[int]:
+    def search(self, query: np.ndarray, radius: float, search_depth: int) -> List[int]:
+        """ Perform clustered search starting at the current cluster.
+
+        :param query: point to search around.
+        :param radius: search radius to consider.
+        :param search_depth: maximum depth to which to search.
+        
+        :return: list of indexes of hits.
+        """
         hits = []
-        if (self.depth < max_depth) and (self.left or self.right):
+        if (self.depth < search_depth) and (self.left or self.right):
             if self.left.can_include(query, radius):
-                hits.extend(self.left.search(query, radius, max_depth))
+                hits.extend(self.left.search(query, radius, search_depth))
             if self.right.can_include(query, radius):
-                hits.extend(self.right.search(query, radius, max_depth))
+                hits.extend(self.right.search(query, radius, search_depth))
         else:
             if self.should_subsample:
                 for i in range(0, len(self.points), self.batch_size):
                     batch = self._get_batch(i)
-                    distances = tf_calculate_distance(query, batch, self.distance_function)
+                    distances = tf_calculate_distance(query, batch, self.df)
                     hits.extend([self.points[i + j] for j, d in enumerate(distances) if d <= radius])
             else:
                 points = [self.data[p] for p in self.points]
-                distances = numpy_calculate_distance(query, points, self.distance_function)
+                distances = numpy_calculate_distance(query, points, self.df)
                 hits.extend([self.points[j] for j, d in enumerate(distances) if d <= radius])
         return hits
