2f86987031ed026644e0254a609043ded5903dcb
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-08-16 13:40:33 -0400

    deeper clustering

--- main.py
+++ main.py
@@ -109,14 +109,14 @@ def benchmark_search(search_object: Search, radius: float, filename: str) -> Non
 if __name__ == '__main__':
     np.random.seed(1234)
     distance_function_ = 'cos'
-    clustering_depth_ = 20
+    clustering_depth_ = 50
 
     times_file = f'logs/times.csv'
     if not os.path.exists(times_file):
         with open(times_file, 'w') as outfile_:
             outfile_.write(f'depth,time,distance_function\n')
 
-    for d in [20]:  # [4, 5, 6, 7, 8, 9, 10, 15, 20]:
+    for d in [30]:  # [4, 5, 6, 7, 8, 9, 10, 15, 20]:
         make_clusters(distance_function=distance_function_, clustering_depth=d, filename=times_file)
         break
 
@@ -127,13 +127,13 @@ if __name__ == '__main__':
     # integer_zip = f'compressed/integer_encodings_{distance_function_}_{clustering_depth_}.zip'
     # search_object_.compress(metadata_filename, integer_filename, integer_zip)
 
-    search_results = f'logs/searches_{distance_function_}_{clustering_depth_}.csv'
-    if not os.path.exists(search_results):
-        with open(search_results, 'w') as outfile_:
-            outfile_.write('success,radius,search_depth,output_size,clusters_searched,'
-                           'linear_time,clustered_time,df_calls\n')
-
-    search_times = f'logs/search_times_{distance_function_}_{clustering_depth_}.csv'
-    for r in [i * (10**-3) for i in [1, 5, 10]]:
-        benchmark_search(search_object_, r, search_results)
+    # search_results = f'logs/searches_{distance_function_}_{clustering_depth_}.csv'
+    # if not os.path.exists(search_results):
+    #     with open(search_results, 'w') as outfile_:
+    #         outfile_.write('success,radius,search_depth,output_size,clusters_searched,'
+    #                        'linear_time,clustered_time,df_calls\n')
+    #
+    # search_times = f'logs/search_times_{distance_function_}_{clustering_depth_}.csv'
+    # for r in [i * (10**-3) for i in [1, 5, 10]]:
+    #     benchmark_search(search_object_, r, search_results)
 
