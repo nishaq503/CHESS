cbe57a015e131935143a3d93848d229be822cf2b
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-11-07 01:45:48 -0500

    writing distance functions

---
+++ tests.py
@@ -0,0 +1,58 @@
+import unittest
+
+import numpy as np
+
+from src.distance_functions import check_input_array, calculate_distances
+
+
+class TestDistanceFunctions(unittest.TestCase):
+    def test_array_attributes(self):
+        x = [1, 1]
+        self.assertRaises(TypeError, check_input_array, x)
+
+        x = np.asarray([1, 1])
+        self.assertRaises(ValueError, check_input_array, x)
+
+        x = np.asarray([])
+        self.assertRaises(ValueError, check_input_array, x)
+
+        x = np.asarray([[]])
+        self.assertRaises(ValueError, check_input_array, x)
+
+        x = np.asarray([[1, 1], [2, 2], [3, 3]])
+        self.assertTrue(check_input_array(x))
+
+    def test_distance_values(self):
+        x = np.asarray([[1, 1], [2, 2], [3, 3]])
+        y = np.asarray([[1, 1], [2, 2], [3, 3]])
+
+        metric = 'euclidean'
+        correct_distances = np.asarray([[0, np.sqrt(2), np.sqrt(8)],
+                                        [np.sqrt(2), 0, np.sqrt(2)],
+                                        [np.sqrt(8), np.sqrt(2), 0]])
+        distances = calculate_distances(x, y, metric)
+        np.testing.assert_almost_equal(distances, correct_distances, decimal=7)
+
+        metric = 'cosine'
+        distances = calculate_distances(x, y, metric)
+        np.testing.assert_almost_equal(distances, np.zeros_like(distances), decimal=7)
+
+        metric = 'hamming'
+        correct_distances = np.asarray([[0, 1, 1],
+                                        [1, 0, 1],
+                                        [1, 1, 0]])
+        distances = calculate_distances(x, y, metric)
+        np.testing.assert_almost_equal(distances, correct_distances, decimal=0)
+
+
+def sandbox():
+    x = np.asarray([[1, 1]])
+    y = np.asarray([[1, 1], [2, 2], [3, 3]])
+    metric = 'euclidean'
+    print(calculate_distances(x, y, metric))
+    return
+
+
+if __name__ == '__main__':
+    # unittest.main()
+    sandbox()
