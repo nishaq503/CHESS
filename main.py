802776bf1d0f85885fa47a65dd6c9a2083c47964
nishaq503 <najib_ishaq@my.uri.edu>
2019-11-08 11:11:43 -0500

    increased min_radius for greengenes data

--- main.py
+++ main.py
@@ -12,26 +12,26 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 if __name__ == '__main__':
     np.random.seed(42)
 
-    initial_depth = 29
+    initial_depth = 1
     globals.MAX_DEPTH = initial_depth
 
     dataset = 'GreenGenes'
     metric = 'hamming'
 
     if dataset == 'GreenGenes':
-        globals.MIN_RADIUS = 1.0 / globals.GREENGENES_NUM_DIMS
+        globals.MIN_RADIUS = 10.0 / globals.GREENGENES_NUM_DIMS
 
     timing_filename = f'logs/clustering_times.csv'
     if not os.path.exists(timing_filename):
         with open(timing_filename, 'a') as outfile:
             outfile.write(f'dataset,metric,starting_depth,ending_depth,time_taken(s)\n')
 
-    # make_clusters(
-    #     dataset=dataset,
-    #     metric=metric,
-    #     depth=initial_depth,
-    #     timing_filename=timing_filename,
-    # )
+    make_clusters(
+        dataset=dataset,
+        metric=metric,
+        depth=initial_depth,
+        timing_filename=timing_filename,
+    )
 
     search_object = read_clusters(
         dataset=dataset,
@@ -54,11 +54,10 @@ if __name__ == '__main__':
             outfile.write(f'depth,radius,correctness,false_negative_rate,num_hits,num_clusters_searched,'
                           f'fraction_searched,df_calls_made,linear_time,chess_time,speedup_factor\n')
 
-    greengenes_min_radius = int(0.01 * globals.GREENGENES_NUM_DIMS)
     radii = {
         'euclidean': [2000, 4000],
         'cosine': [0.005, 0.001],
-        'hamming': [0.001, 0.005],
+        'hamming': [0.001, 0.005, 0.01],
     }
 
     _, queries = get_data_and_queries(dataset)
