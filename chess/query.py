52b46e22b8557b6be658059a1369c1b14d00b144
Tom <thoward12@outlook.com>
2019-11-14 11:29:04 -0500

    Defining a query object.
    Signed-off-by: Tom <thoward12@outlook.com>

---
+++ chess/query.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from chess import globals
+
+
+class Query:
+    def __init__(self,
+                 point: np.ndarray,
+                 radius: globals.RADII_DTYPE = 0,
+                 k: int = 0,
+                 max_depth: int = None
+                 ):
+        self.point = point
+        self.radius = radius
+        self.k = k
+        self.max_depth = max_depth
