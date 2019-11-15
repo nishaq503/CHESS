3ffe754e0f3d1ca06a464b73f52291ccd5dbc6da
Tom <thoward12@outlook.com>
2019-11-14 23:30:23 -0500

    New main.
    Signed-off-by: Tom <thoward12@outlook.com>

---
+++ chess/__main__.py
@@ -0,0 +1,26 @@
+from chess import CHESS
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    import numpy as np
+
+    parser = ArgumentParser('CHESS')
+    parser.add_argument('dataset', help='path to a numpy memmap.')
+    parser.add_argument('metric', help='distance metric from scipy cdist')
+    args = parser.parse_args()
+
+    print("""
+    Welcome to the CHESS Interpreter.
+    """)
+
+    data = np.memmap(args.dataset)
+    chess = CHESS(data, args.metric)
+
+    while True:
+        code = input('> ')
+        try:
+            results = eval(code)
+        except Exception as e:
+            print(e)
+        else:
+            print(results)
