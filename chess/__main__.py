a0d01aa6b85bc5b43d77c17441d9a78bfe69e9b4
Tom <thoward12@outlook.com>
2019-11-19 09:59:38 -0500

    CLI bug fix.
    Signed-off-by: Tom <thoward12@outlook.com>

--- chess/__main__.py
+++ chess/__main__.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     parser = ArgumentParser('CHESS')
     parser.add_argument('dataset', help='path to a numpy memmap.')
     parser.add_argument('metric', help='distance metric from scipy cdist')
+    parser.add_argument('--dtype', nargs=1, type=str, help='datatype of the dataset')
+    parser.add_argument('--shape', nargs='+', type=int, help='shape of the dataset')
     parser.add_argument('--max-depth', nargs=1, type=int, default=defaults.MAX_DEPTH)
     parser.add_argument('--min-radius', nargs=1, type=defaults.RADII_DTYPE, default=defaults.MIN_RADIUS)
     parser.add_argument('--min-points', type=int, nargs=1, default=defaults.MIN_POINTS)
@@ -23,7 +25,7 @@ if __name__ == '__main__':
     Building your CHESS object now.
     """)
 
-    data = np.memmap(args.dataset)
+    data = np.memmap(args.dataset, dtype=args.dtype[0], mode='r', shape=tuple(args.shape))
     chess = CHESS(
         data,
         args.metric,
