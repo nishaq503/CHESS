987ae7bdc2837fc0a54221104b8e780a054de4e6
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-11-21 17:31:35 -0500

    wrote Graph class and tests

---
+++ chess/graph.py
@@ -0,0 +1,72 @@
+from typing import Union, List, Dict, Set
+
+import numpy as np
+
+from chess import defaults
+from chess.cluster import Cluster
+from chess.distance import calculate_distances
+
+
+class Graph:
+    """ Defines the Graph class to investigate leaf-level properties of the data in CHESS. """
+
+    def __init__(
+            self,
+            data: Union[np.memmap, np.ndarray],
+            metric: str,
+            leaves: List[Cluster],
+    ):
+        """ Initializes graph object on leaves.
+
+        :param data: numpy memmap of points in leaves.
+        :param metric: distance function used for clustering.
+        :param leaves: list of leaf clusters with which to build the graph.
+        """
+
+        self.data: np.memmap = data
+        assert len(data.shape) > 1, 'Got 1-d array as data. Expected at least 2-dim array.'
+        assert data.shape[0] > 1, 'Got only one point in data. Expected more points.'
+
+        self.metric: str = metric
+
+        self.leaves: List[Cluster] = leaves
+        assert len(leaves) > 1, 'Got only one leaf. Need more to build a graph.'
+
+        self.graph: Dict[str, Set[str]] = {l.name: set() for l in leaves}
+
+    def build(self):
+        """ Builds the graph by adding edges between leaves that have overlap. """
+        centers = np.asarray([l.center() for l in self.leaves], dtype=defaults.RADII_DTYPE)
+        distances = calculate_distances(centers, centers, self.metric)
+        radii = np.asarray([l.radius() for l in self.leaves], dtype=defaults.RADII_DTYPE)
+        radii_matrix = (np.zeros_like(distances, dtype=defaults.RADII_DTYPE) + radii).T + radii
+        edges = np.sign(distances - radii_matrix)
+        np.fill_diagonal(edges, 0)
+        left, right = tuple(map(list, np.where(edges < 0)))
+        [self.graph[self.leaves[l].name].add(self.leaves[r].name) for l, r in zip(left, right)]
+        return
+
+    def connected_components(self) -> List[Set[Cluster]]:
+        """ Finds the connected components in the graph.
+
+        :return: List of sets of leaf names where each set is a connected component
+        """
+        def dft(start):
+            visited_, stack = set(), [start]
+            while stack:
+                vertex = stack.pop()
+                if vertex not in visited_:
+                    visited_.add(vertex)
+                    if vertex in self.graph.keys():
+                        stack.extend(self.graph[vertex] - visited_)
+            return visited_
+
+        unvisited: Set[str] = set(self.graph.keys())
+        components: List[Set[Cluster]] = []
+
+        while unvisited:
+            visited = dft(unvisited.pop())
+            components.append(visited.copy())
+            unvisited -= visited
+
+        return components
---
+++ chess/tests/test_graph.py
@@ -0,0 +1,82 @@
+import tempfile
+import unittest
+from typing import List
+
+import numpy as np
+from chess import CHESS, defaults
+from chess.cluster import Cluster
+from chess.graph import Graph
+
+
+class TestGraph(unittest.TestCase):
+    tempfile: str
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        np.random.seed(42)
+        cls.tempfile = tempfile.NamedTemporaryFile()
+
+        scale, num_points = 12, 10_000
+        samples = scale * (np.random.rand(2, num_points) - 0.5)
+        distances = np.linalg.norm(samples, axis=0)
+        x = [samples[0, i] for i in range(num_points)
+             if distances[i] < 6 and (distances[i] > 4 or distances[i] < 2)]
+        y = [samples[1, i] for i in range(num_points)
+             if distances[i] < 6 and (distances[i] > 4 or distances[i] < 2)]
+
+        data = np.asarray((x, y), dtype=defaults.RADII_DTYPE).T
+        cls.data = np.memmap(cls.tempfile, dtype='float32', mode='w+', shape=data.shape)
+        cls.data[:] = data[:]
+
+        cls.chess_object: CHESS = CHESS(
+            data=data,
+            metric='euclidean',
+            max_depth=25,
+            min_points=1,
+            min_radius=defaults.RADII_DTYPE(0)
+        )
+        cls.chess_object.build()
+        cls.max_depth = max(map(len, cls.chess_object.root.dict().keys()))
+        return
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        return
+
+    def test_init(self):
+        np.random.seed(42)
+        self.assertEqual(17, self.max_depth)
+        for d in range(1, self.max_depth + 1):
+            leaves: List[Cluster] = list(self.chess_object.root.leaves(d))
+            graph: Graph = Graph(
+                data=self.data,
+                metric='euclidean',
+                leaves=leaves
+            )
+            self.assertEqual(self.data.shape[0], graph.data.shape[0])
+            self.assertEqual(self.data.shape[1], graph.data.shape[1])
+            self.assertEqual('euclidean', graph.metric)
+            self.assertEqual(len(leaves), len(list(graph.graph.keys())))
+            self.assertFalse(any(graph.graph.values()))
+        return
+
+    def test_connected_components(self):
+        np.random.seed(42)
+        num_components = [1]
+        for d in range(1, self.max_depth + 1):
+            leaves: List[Cluster] = list(self.chess_object.root.leaves(d))
+            graph: Graph = Graph(
+                data=self.data,
+                metric='euclidean',
+                leaves=leaves
+            )
+            graph.build()
+            self.assertEqual(len(leaves), len(list(graph.graph.keys())))
+
+            components = graph.connected_components()
+            if d > 1:
+                num_components.append(len(components))
+            self.assertTrue(num_components[d - 1] <= len(components))
+            if d == self.max_depth:
+                self.assertEqual(len(components), self.data.shape[0])
+        return
