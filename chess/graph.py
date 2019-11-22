cd3c7f93f9015b8fcaa87af104dd07068b96552e
Tom <thoward12@outlook.com>
2019-11-22 16:34:03 -0500

    Refactor to functions.
    Signed-off-by: Tom <thoward12@outlook.com>

--- chess/graph.py
+++ chess/graph.py
@@ -1,4 +1,10 @@
-from typing import Union, List, Dict, Set
+""" Graph Functions.
+
+TODO: Think about how to use number of components into a good stopping condition.
+TODO: Implement max-flow and min-cut.
+TODO: Implement methods to find bottlenecks and "almost disconnected components"
+"""
+from typing import List, Dict, Set
 
 import numpy as np
 
@@ -7,73 +13,40 @@ from chess.cluster import Cluster
 from chess.distance import calculate_distances
 
 
-class Graph:
-    """ Defines the Graph class to investigate leaf-level properties of the data in CHESS. """
+def graph(clusters: List[Cluster]) -> Dict:
+    """ Builds and returns a graph from the given list of clusters. """
+    g = {l.name: set() for l in clusters}
+    centers = np.asarray([l.center() for l in clusters], dtype=defaults.RADII_DTYPE)
+    distances = calculate_distances(centers, centers, clusters[0].metric)
+    radii = np.asarray([l.radius() for l in clusters], dtype=defaults.RADII_DTYPE)
+    radii_matrix = (np.zeros_like(distances, dtype=defaults.RADII_DTYPE) + radii).T + radii
+    np.fill_diagonal(radii_matrix, 0)
+    left, right = tuple(map(list, np.where((distances - radii_matrix) < 0)))
+    # TODO: Should we store clusters here, or just name?
+    [g[clusters[l].name].add(clusters[r].name) for l, r in zip(left, right)]
+    return g
 
-    def __init__(
-            self,
-            data: Union[np.memmap, np.ndarray],
-            metric: str,
-            leaves: List[Cluster],
-    ):
-        """ Initializes graph object on leaves.
 
-        :param data: numpy memmap of points in leaves.
-        :param metric: distance function used for clustering.
-        :param leaves: list of leaf clusters with which to build the graph.
-        """
+def depth_first_traversal(graph, start) -> Set[str]:
+    """ Depth first traversal. """
+    visited, stack = set(), [start]
+    while stack:
+        vtx = stack.pop()
+        if vtx not in visited:
+            visited.add(vtx)
+            stack.extend(graph[vtx] - visited)
+    return visited
 
-        self.data: np.memmap = data
-        assert len(data.shape) > 1, 'Got 1-d array as data. Expected at least 2-dim array.'
-        assert data.shape[0] > 1, 'Got only one point in data. Expected more points.'
 
-        self.metric: str = metric
+def connected_components(graph) -> List[Set[str]]:
+    """ Returns all connected components from the given list of clusters. """
 
-        self.leaves: List[Cluster] = leaves
-        assert len(leaves) > 1, 'Got only one leaf. Need more to build a graph.'
+    unvisited: Set[str] = set(graph.keys())
+    components: List[Set[str]] = []
 
-        self.graph: Dict[str, Set[str]] = {}
-        self.components: List[Set[Cluster]] = []
+    while unvisited:
+        visited = depth_first_traversal(graph, unvisited.pop())
+        components.append(visited.copy())
+        unvisited -= visited
 
-    def build(self):
-        """ Builds the graph by adding edges between leaves that have overlap. """
-        self.graph = {l.name: set() for l in self.leaves}
-        centers = np.asarray([l.center() for l in self.leaves], dtype=defaults.RADII_DTYPE)
-        distances = calculate_distances(centers, centers, self.metric)
-        radii = np.asarray([l.radius() for l in self.leaves], dtype=defaults.RADII_DTYPE)
-        radii_matrix = (np.zeros_like(distances, dtype=defaults.RADII_DTYPE) + radii).T + radii
-        np.fill_diagonal(radii_matrix, 0)
-        left, right = tuple(map(list, np.where((distances - radii_matrix) < 0)))
-        [self.graph[self.leaves[l].name].add(self.leaves[r].name) for l, r in zip(left, right)]
-        return
-
-    def connected_components(self) -> List[Set[Cluster]]:
-        """ Finds the connected components in the graph.
-
-        :return: List of sets of leaf names where each set is a connected component
-        """
-        if len(self.components) == 0:
-            def dft(start):
-                visited_, stack = set(), [start]
-                while stack:
-                    vertex = stack.pop()
-                    if vertex not in visited_:
-                        visited_.add(vertex)
-                        if vertex in self.graph.keys():
-                            stack.extend(self.graph[vertex] - visited_)
-                return visited_
-
-            unvisited: Set[str] = set(self.graph.keys())
-            components: List[Set[Cluster]] = []
-
-            while unvisited:
-                visited = dft(unvisited.pop())
-                components.append(visited.copy())
-                unvisited -= visited
-
-            self.components = components
-        return self.components
-
-    # TODO: Think about how to use number of components into a good stopping condition.
-    # TODO: Implement max-flow and min-cut.
-    # TODO: Implement methods to find bottlenecks and "almost disconnected components"
+    return components
