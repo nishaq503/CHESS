a2021e2aee4a1aea089254e97f9691ef9231089a
Tom <thoward12@outlook.com>
2019-12-03 21:35:00 -0500

    Adding functional tests to manifold
    Signed-off-by: Tom <thoward12@outlook.com>

--- tests/test_manifold.py
+++ tests/test_manifold.py
@@ -1,6 +1,47 @@
 import unittest
 
+from chess import criterion
 from chess.manifold import *
+
+
+class TestManifoldFunctional(unittest.TestCase):
+    def test_random(self):
+        # We begin by getting some data and building with no constraints.
+        data = np.random.randn(1000, 3)
+        m = Manifold(data, 'euclidean')
+        m.build()
+        # With no constraints, clusters should be singletons.
+        self.assertEqual(len(m.find_clusters(data[0], 0.0, -1)), 1)
+        self.assertEqual(len(m.find_points(data[0], radius=0.0)), 1)
+
+        m.build(criterion.MinPoints(10))
+        c = m.find_clusters(data[0], 0.0, -1)[0]
+        self.assertEqual(len(m.find_points(c.center, c.radius)), 10)
+        return
+
+    def test_all_same(self):
+        # A bit simpler, every point is the same.
+        data = np.ones(1000, 3)
+        m = Manifold(data, 'euclidean')
+        m.build()
+        # There should only ever be one cluster here.
+        self.assertEqual(1, len(m.graphs))
+        m.deepen()
+        # Even after explicit deepen calls.
+        self.assertEqual(1, len(m.graphs))
+        self.assertEqual(1, len(m.find_clusters(data[0], 0.0, -1)))
+        # And, we should get all 1000 points back for any of the data.
+        self.assertEqual(1000, len(m.find_points(data[0], 0.0)))
+        return
+
+    def test_two_clumps(self):
+        # Here we have two distinct clusters.
+        data = np.concatenate([np.ones(500, 2) * -2, np.ones(500, 2) * 2])
+        m = Manifold(data, 'euclidean')
+        # We expect building to stop with two clusters.
+        m.build()
+        self.assertEqual(2, len(m.graphs[-1]))
+        return
 
 
 class TestManifold(unittest.TestCase):
