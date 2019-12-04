3a251092658b2e2ae19fa33ab207ba4f4886339d
Tom <thoward12@outlook.com>
2019-12-04 09:52:04 -0500

    Test MinRadius
    Signed-off-by: Tom <thoward12@outlook.com>

---
+++ tests/test_criterion.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+from chess.criterion import *
+
+
+class TestCriterion(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.data = np.random.randn(100, 2)
+        return
+
+    def setUp(self) -> None:
+        self.manifold = Manifold(self.data, 'euclidean')
+        return
+
+    def test_min_radius(self):
+        min_radius = 1.
+        self.manifold.build(MinRadius(min_radius))
+        self.assertTrue(all([c.radius >= min_radius for g in self.manifold for c in g]))
