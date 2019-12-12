64a7fa32de23d1b4c2f142a7797370f266e33174
Najib Ishaq <najib_ishaq@my.uri.edu>
2019-12-12 06:25:11 -0800

    generated new single-query video

--- benchmarks/visualizations.py
+++ benchmarks/visualizations.py
@@ -1,3 +1,4 @@
+from time import time
 from typing import Dict, Tuple, List
 
 import numpy as np
@@ -46,25 +47,25 @@ def get_data(dataset: str) -> np.memmap:
     )
 
 
-def plot(angles, data_, labels, folder, figsize, dpi, s, title):
-    x, y, z = data_[:, 0], data_[:, 1], data_[:, 2]
-    x_limits = [int(np.min(x)), int(np.max(x))]
-    y_limits = [int(np.min(y)), int(np.max(y))]
-    z_limits = [int(np.min(z)), int(np.max(z))]
+def plot(angles, data, limits, labels, folder, figsize, dpi, s, title):
+    x, y, z = data[:, 0], data[:, 1], data[:, 2]
     plt.clf()
     plt.close('all')
     fig = plt.figure(figsize=figsize, dpi=dpi)
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(x, y, z, c=labels, s=s, cmap='Set1')
     plt.title(title)
-    ax.set_xlim(x_limits)
-    ax.set_ylim(y_limits)
-    ax.set_zlim(z_limits)
+    ax.set_xlim(limits[0])
+    ax.set_ylim(limits[1])
+    ax.set_zlim(limits[2])
     plt.gca().set_axis_off()
     plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
     plt.margins(0, 0, 0)
     for azimuth in range(angles[0], angles[1]):
         ax.view_init(elev=10, azim=azimuth)
+        plt.savefig(folder + f'{benchmarks.PLOT_NUMBER:05d}.png', bbox_inches='tight', pad_inches=0)
+        benchmarks.PLOT_NUMBER += 1
+        ax.view_init(elev=10, azim=azimuth + 0.5)
         plt.savefig(folder + f'{benchmarks.PLOT_NUMBER:05d}.png', bbox_inches='tight', pad_inches=0)
         benchmarks.PLOT_NUMBER += 1
         # break  # TODO: remove
@@ -93,55 +94,70 @@ def query_plots(
         depth_to_clusters: Dict[int, Tuple[List[Cluster], List[Cluster]]],
         base_path: str,
 ):
-    angle, step = 0, 15
-    for d in range(0, 41):
-        if d < 15:
-            continue
+    limits = (
+        [int(np.min(umap_data[:, 0])), int(np.max(umap_data[:, 0]))],
+        [int(np.min(umap_data[:, 1])), int(np.max(umap_data[:, 1]))],
+        [int(np.min(umap_data[:, 2])), int(np.max(umap_data[:, 2]))],
+    )
+    angle, step = 0, 30
+    depths = [0] * 6
+    depths.extend(range(0, 51))
+    fractions = {}
+    for d in depths:
         if depth_to_clusters:
-            gray, green = depth_to_clusters[d]
-            gray_argpoints: List[int] = [int(p) for cluster in gray for p in cluster.argpoints]
-            gray_points = umap_data[gray_argpoints]
-            gray_labels = [8 if g != argquery else 0 for g in gray_argpoints]
-            if len(gray_labels) >= 9:
-                for g in range(9):
-                    if gray_argpoints[g] == argquery:
-                        gray_labels[9] = g
-                    else:
-                        gray_labels[g] = g
-            fraction: float = len(gray_argpoints) / 100_000
-            plot(
-                angles=(angle, angle + step),
-                data_=gray_points,
-                labels=gray_labels,
-                folder=base_path,
-                figsize=(12, 12),
-                dpi=150,
-                s=2. if fraction < 0.1 else 0.1,
-                title=f'depth: {d}, fraction: {fraction:.6f}'
-            )
-            angle += step
-            green_argpoints: List[int] = [int(p) for cluster in green for p in cluster.argpoints]
-            green_set = set(green_argpoints)
-            gray_labels = [2 if g in green_set else 8 for g in gray_argpoints]
-            if len(gray_labels) >= 9:
-                for g in range(9):
-                    if gray_argpoints[g] == argquery:
-                        gray_labels[9] = g
-                    else:
-                        gray_labels[g] = g
-            plot(
-                angles=(angle, angle + step),
-                data_=gray_points,
-                labels=gray_labels,
-                folder=base_path,
-                figsize=(12, 12),
-                dpi=150,
-                s=2. if fraction < 0.1 else 0.1,
-                title=f'depth: {d}, fraction: {fraction:.6f}'
-            )
-            angle += step
+            if d in depth_to_clusters:
+                gray, green = depth_to_clusters[d]
+                gray_argpoints: List[int] = [int(p) for cluster in gray for p in cluster.argpoints]
+                gray_points = umap_data[gray_argpoints]
+                gray_labels = [8 if g != argquery else 0 for g in gray_argpoints]
+                fractions[d] = len(gray_argpoints) / 100_000
+                print(f'depth: {d}, fraction: {fractions[d]:.6f}')
+                if len(gray_labels) >= 9:
+                    for g in range(9):
+                        if gray_argpoints[g] == argquery:
+                            gray_labels[9] = g
+                        else:
+                            gray_labels[g] = g
+                title = '100,000 points from the APOGEE data under UMAP dimensionality reduction'
+                if d != 0:
+                    title = f'depth: {d}, fraction: {fractions[d]:.6f}'
+                plot(
+                    angles=(angle, angle + step),
+                    data=gray_points,
+                    limits=limits,
+                    labels=gray_labels,
+                    folder=base_path,
+                    figsize=(12, 12),
+                    dpi=150,
+                    s=2. if fractions[d] < 0.1 else 0.1,
+                    title=title
+                )
+                angle += step
+                if d != 0:
+                    green_argpoints: List[int] = [int(p) for cluster in green for p in cluster.argpoints]
+                    green_set = set(green_argpoints)
+                    gray_labels = [2 if g in green_set else 8 for g in gray_argpoints]
+                if len(gray_labels) >= 9:
+                    for g in range(9):
+                        if gray_argpoints[g] == argquery:
+                            gray_labels[9] = g
+                        else:
+                            gray_labels[g] = g
+                plot(
+                    angles=(angle, angle + step),
+                    data=gray_points,
+                    limits=limits,
+                    labels=gray_labels,
+                    folder=base_path,
+                    figsize=(12, 12),
+                    dpi=150,
+                    s=2. if fractions[d] < 0.1 else 0.1,
+                    title=title
+                )
+                angle += step
         else:
             break
+    # print(f'argquery: {argquery}, fractions: {sorted(list(fractions.items()))}')
     return
 
 
@@ -159,19 +175,24 @@ def make_plots(
     else:
         raise ValueError(f'dataset must be either apogee or greengenes. Got {dataset} instead.')
     for q in meta_data.keys():
-        depth_to_cluster_names: Dict[int, Tuple[List[str], List[str]]] = meta_data[q]
-        depth_to_clusters: Dict[int, Tuple[List[Cluster], List[Cluster]]] = dict()
-        for k, v in depth_to_cluster_names.items():
-            grays = [manifold.select(c) for c in v[0]]
-            greens = [manifold.select(c) for c in v[1]]
-            depth_to_clusters[k] = (grays, greens)
-        query_plots(
-            umap_data=embedding,
-            argquery=q,
-            depth_to_clusters=depth_to_clusters,
-            base_path=plot_folder
-        )
-        break  # TODO: remove
+        if q == 6265:
+            depth_to_cluster_names: Dict[int, Tuple[List[str], List[str]]] = meta_data[q]
+            depth_to_clusters: Dict[int, Tuple[List[Cluster], List[Cluster]]] = dict()
+            for k, v in depth_to_cluster_names.items():
+                try:
+                    grays = [manifold.select(c) for c in v[0]]
+                    greens = [manifold.select(c) for c in v[1]]
+                except AssertionError:
+                    continue
+                depth_to_clusters[k] = (grays, greens)
+            else:
+                query_plots(
+                    umap_data=embedding,
+                    argquery=q,
+                    depth_to_clusters=depth_to_clusters,
+                    base_path=plot_folder
+                )
+            # break  # TODO: remove
     return
 
 
@@ -195,7 +216,7 @@ def get_metadata(dataset: str) -> Dict[int, Dict[int, Tuple[List[str], List[str]
         temp_df.drop(labels='query_radius', axis=1, inplace=True)
 
         df_by_argquery[aq] = dict()
-        for d in range(0, 41):
+        for d in range(0, 51):
             d_df: pd.DataFrame = temp_df[temp_df.current_depth == d]
             d_df.reset_index(drop=True, inplace=True)
             names: List[List[str]] = [list(), list()]
@@ -209,12 +230,14 @@ def get_metadata(dataset: str) -> Dict[int, Dict[int, Tuple[List[str], List[str]
 
 def get_manifold(dataset: str) -> Manifold:
     data = get_data(dataset)
+    s = time()
     if dataset == 'apogee':
         with open('manifold_logs/chess_apogee2_100k_50.pickle', 'rb') as infile:
             manifold: Manifold = Manifold.pickle_load(infile, data)
     elif dataset == 'greengenes':
         with open('manifold_logs/chess_gg100k_50.pickle', 'rb') as infile:
             manifold: Manifold = Manifold.pickle_load(infile, data)
+    print(f'reading {dataset} manifold from pickle took {time() - s:.6f} seconds.')
 
     manifold.__dict__['propagate'] = False
     manifold.__dict__['calculate_neighbors'] = False
@@ -222,7 +245,7 @@ def get_manifold(dataset: str) -> Manifold:
 
 
 def main():
-    dataset = 'greengenes'
+    dataset = 'apogee'
     manifold: Manifold = get_manifold(dataset=dataset)
     meta_data = get_metadata(dataset=dataset)
     make_plots(dataset=dataset, manifold=manifold, meta_data=meta_data)
